8135368ad610364414eb6e3c54d09fd3d230a0e0
anasyusef <anas.y0807@gmail.com>
2022-03-28 23:34:38 +0100

    perf: add sparse order-book benchmarking

--- src/bench/Main.java
+++ src/bench/Main.java
@@ -13,5 +13,14 @@ public class Main {
         mePerf.benchmarkAddOrdersDenseOrderBook(320_000);
         mePerf.benchmarkAddOrdersDenseOrderBook(640_000);
         mePerf.benchmarkAddOrdersDenseOrderBook(1_280_000);
+        System.out.println();
+        mePerf.benchmarkAddOrdersSparseOrderBook(10_000);
+        mePerf.benchmarkAddOrdersSparseOrderBook(20_000);
+        mePerf.benchmarkAddOrdersSparseOrderBook(40_000);
+        mePerf.benchmarkAddOrdersSparseOrderBook(80_000);
+        mePerf.benchmarkAddOrdersSparseOrderBook(160_000);
+        mePerf.benchmarkAddOrdersSparseOrderBook(320_000);
+        mePerf.benchmarkAddOrdersSparseOrderBook(640_000);
+        mePerf.benchmarkAddOrdersSparseOrderBook(1_280_000);
     }
 }
--- src/bench/MatchingEnginePerformance.java
+++ src/bench/MatchingEnginePerformance.java
@@ -25,10 +25,33 @@ public class MatchingEnginePerformance {
         return orders;
     }
 
+    /**
+     * Benchmarks adding orders to a dense (prices are much closer to each other and there is more depth at each level)
+     * order-book.
+     * @param num_orders Number of orders to generate
+     */
     public void benchmarkAddOrdersDenseOrderBook(int num_orders) {
-        // Tests matching engine with close prices and more depth on each level;
         double minPriceRange = 0.01;
-        double maxPriceRange = 2;
+        double maxPriceRange = 1;
+        long executionTime = benchmarkAddOrders(num_orders, minPriceRange, maxPriceRange);
+        System.out.println("Adding " + num_orders + " orders using a dense order-book - Execution time: "
+                + executionTime + " milliseconds");
+    }
+
+    /**
+     * Benchmarks adding orders to a sparse
+     * (prices are further apart from each other and there is less depth at each level) order-book
+     * @param num_orders Number of orders to generate
+     */
+    public void benchmarkAddOrdersSparseOrderBook(int num_orders) {
+        double minPriceRange = 1;
+        double maxPriceRange = 100;
+        long executionTime = benchmarkAddOrders(num_orders, minPriceRange, maxPriceRange);
+        System.out.println("Adding " + num_orders + " orders using a sparse order-book - Execution time: "
+                + executionTime + " milliseconds");
+    }
+
+    private long benchmarkAddOrders(int num_orders, double minPriceRange, double maxPriceRange) {
         Order[] orders = generateOrders(num_orders, minPriceRange, maxPriceRange);
         long startTime = System.currentTimeMillis();
         MatchingEngine me = new MatchingEngine("LINK/USDT");
@@ -36,13 +59,7 @@ public class MatchingEnginePerformance {
             me.add(order);
         }
         long endTime = System.currentTimeMillis();
-        System.out.println("Adding " + num_orders + " orders using a dense order-book - Execution time: "
-                + (endTime - startTime) + " milliseconds");
-    }
-
-    public static void sparseOrderBookTest(int orders) {
-        // Tests matching engine with less depth on each level and more prices scattered around
-
+        return endTime - startTime;
     }
 
     public static void orderBookReportTest(int orders) {
