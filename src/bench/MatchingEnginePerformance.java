3bbaaa1c9625e406c24a9ae0e268f4e89baaac11
anasyusef <anas.y0807@gmail.com>
2022-03-28 20:17:14 +0100

    wip: implement benchmarking

---
+++ src/bench/MatchingEnginePerformance.java
@@ -0,0 +1,18 @@
+package bench;
+
+public class MatchingEnginePerformance {
+
+    public static void denseOrderBookTest(int orders) {
+        // Tests matching engine with close prices and more depth on each level;
+//        Math.random()
+    }
+
+    public static void sparseOrderBookTest(int orders) {
+        // Tests matching engine with less depth on each level and more prices scattered around
+
+    }
+
+    public static void orderBookReportTest(int orders) {
+        // Tests matching engine reporting more market depth. Benchmarks market depth
+    }
+}
